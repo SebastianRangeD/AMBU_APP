327d9385125c7af9ccf9e9d40c922739814ac949
Sebastian CHG <sebokiabot@gmail.com>
2024-04-25 11:11:54 -0600

    Login - file created for Gera to work with it

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+/*
+ACTIVIDAD A COMPLETAR:
+
+Parte 1: Con los componentes de react native crear un pequeño form de dos
+inputs de texto (Usuario y contraseña) y un botón (Iniciar sesión) para tener la estructura base del login.
+
+Parte 2: Utilizando los hooks correspondientes (Lo más probable es que sean useState o useRef).
+Hacer que esos hooks almacenen el valor del usuario y la contraseña cada que se modifique el
+contenido de los inputs.
+
+Parte 3: Al dar click en el botón de iniciar sesión, que compare las credenciales ingresadas por el usuario
+con las que te dejo hardcodeadas, en caso de coincidencia manda un alert con el mensaje ('Inicio de sesión 
+exitoso'), caso contrario, un alert con el mensaje (`Inicio de sesión fallido: ${Campo que no coincidió}`).
+ */
+
+const credentials = {
+    user: 'admin@ambu.mx',
+    password: 'Admin.2024!',
+}
+
+const LoginForm = () => {
+    return (
+        <></> //Esta linea la puedes borrar y escribir tu código Gera
+    );
+}
+
+export default LoginForm;
