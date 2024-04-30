aac247b5f97f552a37bf1af03710114d61bb8c67
Gerardo Diaz <a20310069@live.ceti.mx>
2024-04-29 18:39:23 -0600

    Gera - task finished

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { TextInput, View, Text, Button, Alert, StyleSheet} from 'react-native';
 
 /*
 ACTIVIDAD A COMPLETAR:
@@ -15,15 +16,71 @@ con las que te dejo hardcodeadas, en caso de coincidencia manda un alert con el 
 exitoso'), caso contrario, un alert con el mensaje (`Inicio de sesión fallido: ${Campo que no coincidió}`).
  */
 
+
 const credentials = {
     user: 'admin@ambu.mx',
     password: 'Admin.2024!',
-}
+};
 
 const LoginForm = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    const alertLogin = () => {
+        if (username === credentials.user && password === credentials.password){
+            Alert.alert('Inicio de sesion exitoso');
+        }else if(username !== credentials.user && password !== credentials.password){
+            Alert.alert('Inicio de sesion fallido: usuario y contraseña incorrectos');
+        }else if(username !== credentials.user){
+            Alert.alert('Inicio de sesion fallido: usuario incorrecto');
+        }else{
+            Alert.alert('Inicio de sesion fallido: contraseña incorrecta');
+        }
+    };
     return (
-        <></> //Esta linea la puedes borrar y escribir tu código Gera
+        <View style={styles.container}>
+            <Text style={styles.title}>Park Traiking</Text>
+            <TextInput 
+                placeholder='Usuario' 
+                style={styles.textInput} 
+                onChangeText={text => setUsername(text)}/>
+            <TextInput 
+                placeholder='Contraseña' 
+                style={styles.textInput} 
+                onChangeText={text => setPassword(text)} 
+                secureTextEntry={true}/>
+            <Button style={styles.button} title='Iniciar Sesion' onPress={alertLogin}/>
+        </View>
     );
-}
+};
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    title:{
+        fontSize: 65,
+        color: 'black',
+        fontWeight: 'bold',
+    },
+    textInput:{
+        padding:10,
+        paddingStart:30,
+        width:'80%',
+        height:50,
+        marginTop:20,
+        borderRadius:30
+    },
+    button:{
+        width:'80%',
+        height:50,
+        borderRadius:25,
+        padding:10,
+        alignItems:'center',
+        justifyContent:'center'
+    }
+});
 
 export default LoginForm;
